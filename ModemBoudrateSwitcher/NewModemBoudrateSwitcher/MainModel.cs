f87934d2cbc42270129f8dd2e75e794769c46e40
liroy machluf <liroyma@gmail.com>
2018-11-14 10:29:09 +0000

    Update ModemBoudrateSwitcher/NewModemBoudrateSwitcher/MainModel.cs

--- ModemBoudrateSwitcher/NewModemBoudrateSwitcher/MainModel.cs
+++ ModemBoudrateSwitcher/NewModemBoudrateSwitcher/MainModel.cs
@@ -1,6 +1,8 @@
 ﻿using NewModemBoudrateSwitcher.Helpers;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO.Ports;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,11 +11,23 @@ namespace NewModemBoudrateSwitcher
 {
     public class MainModel: ViewModel
     {
+        private ObservableCollection<string> _ports  = new ObservableCollection<string>();
+        public ObservableCollection<string> Ports
+        {
+            get { return _ports; }
+            set { SetProperty(ref _ports, value); }
+        }
+
         private string text = "REE";
         public string Text
         {
             get { return text; }
             set { SetProperty(ref text, value); }
+        }
+
+        public MainModel()
+        {
+            string[] ports = SerialPort.GetPortNames();
         }
 
     }
